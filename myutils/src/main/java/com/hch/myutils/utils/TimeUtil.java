8c0aa826ff38707ae7108ba78190b904a54b5f1e
DESKTOP-CB6NPJE\VR10054 <517073365@qq.com>
2019-04-03 14:41:44 +0800

    单例时间管理类

--- myutils/src/main/java/com/hch/myutils/utils/TimeUtil.java
+++ myutils/src/main/java/com/hch/myutils/utils/TimeUtil.java
@@ -14,20 +14,39 @@ import java.util.Calendar;
  */
 public class TimeUtil {
 
-    private static boolean isStart = false;
+    private static TimeUtil timeUtil = null;
 
-    public static boolean isCurrentInTimeScope(int beginHour, int endHour) {
-        return isCurrentInTimeScope(beginHour,0,endHour,0);
+    // 双重检查
+    public static TimeUtil getInstance() {
+        if (timeUtil == null) {
+            synchronized (WifiAdminUtil.class) {
+                if (timeUtil == null) {
+                    timeUtil = new TimeUtil();
+                }
+            }
+        }
+        return timeUtil;
     }
-    public static boolean isCurrentInTimeScope(int beginHour, int beginMin, int endHour, int endMin) {
-        return isCurrentInTimeScope(beginHour,beginMin,0,endHour,endMin,0);
+
+    // 构造器
+    private TimeUtil() {
     }
-    public static boolean isCurrentInTimeScope(int beginHour, int beginMin, int beginSecond, int endHour, int endMin, int endSecond) {
-        return isCurrentInTimeScope(beginHour,beginMin,beginSecond,0,endHour,endMin,endSecond,0);
+
+    private boolean isStart = false;
+
+    public boolean isCurrentInTimeScope(int beginHour, int endHour) {
+        return isCurrentInTimeScope(beginHour, 0, endHour, 0);
     }
+
+    public boolean isCurrentInTimeScope(int beginHour, int beginMin, int endHour, int endMin) {
+        return isCurrentInTimeScope(beginHour, beginMin, 0, endHour, endMin, 0);
+    }
+
+    public boolean isCurrentInTimeScope(int beginHour, int beginMin, int beginSecond, int endHour, int endMin, int endSecond) {
+        return isCurrentInTimeScope(beginHour, beginMin, beginSecond, 0, endHour, endMin, endSecond, 0);
+    }
+
     /**
-     * @Description: TODO 判断当前时间是否在指定时间段内
-     * @author : hechuang
      * @param : beginHour 开始时
      * @param : beginMin 开始分
      * @param : beginSecond 开始秒
@@ -38,8 +57,10 @@ public class TimeUtil {
      * @param : endMilli 结束毫秒
      * @return :
      * created at 2018/10/25 15:33
+     * @Description: TODO 判断当前时间是否在指定时间段内
+     * @author : hechuang
      */
-    public static boolean isCurrentInTimeScope(int beginHour, int beginMin, int beginSecond, int beginMilli, int endHour, int endMin, int endSecond, int endMilli) {
+    public boolean isCurrentInTimeScope(int beginHour, int beginMin, int beginSecond, int beginMilli, int endHour, int endMin, int endSecond, int endMilli) {
         long aDayInMillis = 1000 * 60 * 60 * 24;
         long spe1 = getSpeDate(beginHour, beginMin, beginSecond, beginMilli); // 09:30 分时时间
         long spe2 = getSpeDate(endHour, endMin, endSecond, endMilli) + aDayInMillis; // 11:30 分时时间
@@ -56,8 +77,8 @@ public class TimeUtil {
      * @param milliSecond
      * @return
      */
-    public static long getSpeDate(int hour, int minute, int second,
-                                   int milliSecond) {
+    public long getSpeDate(int hour, int minute, int second,
+                           int milliSecond) {
 
         Calendar cal = Calendar.getInstance();
 
@@ -72,48 +93,51 @@ public class TimeUtil {
         return cal.getTimeInMillis();
     }
 
-    private static int countDownNum;
-    private static CountDownTimeListener mCountDownTimeListener;
+    private int countDownNum;
+    private CountDownTimeListener mCountDownTimeListener;
 
     /**
-      * @Description: TODO 开启倒计时 重复调用则是重置计时器
-      * @author hechuang
-      * @param
-      * @return    返回类型
-      * @create 2019/2/22
-      * @throws
-      */
-    public static void startCountDown(int timeNum, CountDownTimeListener countDownTimeListener){
+     * @param
+     * @return 返回类型
+     * @throws
+     * @Description: TODO 开启倒计时 重复调用则是重置计时器
+     * @author hechuang
+     * @create 2019/2/22
+     */
+    public void startCountDown(int timeNum, CountDownTimeListener countDownTimeListener) {
         countDownNum = timeNum;
-        if(!isStart){
+        if (!isStart) {
             mCountDownTimeListener = countDownTimeListener;
             handler.postDelayed(runnable, 1000);
         }
     }
+
     /**
-      * @Description: TODO 停止倒计时
-      * @author hechuang
-      * @param
-      * @return    返回类型
-      * @create 2019/3/29
-      * @throws
-      */
-    public static  void stopCountDown(){
+     * @param
+     * @return 返回类型
+     * @throws
+     * @Description: TODO 停止倒计时
+     * @author hechuang
+     * @create 2019/3/29
+     */
+    public void stopCountDown() {
+        if (isStart) {
+            handler.removeCallbacks(runnable);
+        }
         isStart = false;
-        handler.removeCallbacks(runnable);
     }
 
 
-    private static Handler handler = new Handler();
-    private static Runnable runnable = new Runnable() {
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable() {
         @Override
         public void run() {
             countDownNum--;
-            if(countDownNum > -1){
+            if (countDownNum > -1) {
                 isStart = true;
                 mCountDownTimeListener.surplusTime(countDownNum);
                 handler.postDelayed(this, 1000);
-            }else{
+            } else {
                 isStart = false;
                 handler.removeCallbacks(this);
             }
@@ -121,14 +145,14 @@ public class TimeUtil {
     };
 
     /**
-      * @Description: TODO 获取定时器是否开启
-      * @author hechuang
-      * @param
-      * @return    返回类型
-      * @create 2019/3/29
-      * @throws
-      */
-    public static boolean isStart(){
+     * @param
+     * @return 返回类型
+     * @throws
+     * @Description: TODO 获取定时器是否开启
+     * @author hechuang
+     * @create 2019/3/29
+     */
+    public boolean isStart() {
         return isStart;
     }
 }
