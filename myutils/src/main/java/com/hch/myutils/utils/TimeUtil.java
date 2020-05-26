23c60524b275df5c1baea7dd14712dbfd15ac3e8
hechuang <517073365@qq.com>
2020-05-26 10:08:44 +0800

    添加各时间格式转换

--- myutils/src/main/java/com/hch/myutils/utils/TimeUtil.java
+++ myutils/src/main/java/com/hch/myutils/utils/TimeUtil.java
@@ -4,7 +4,9 @@ import android.os.Handler;
 
 import com.hch.myutils.interfaces.CountDownTimeListener;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * @author hechuang
@@ -154,5 +156,35 @@ public class TimeUtil {
      */
     public boolean isStart() {
         return isStart;
+    }
+
+    /**
+     * 时间戳转换成日期格式字符串
+     * @param seconds 精确到秒的字符串
+     * @return
+     */
+    public static String timeStamp2Date(String seconds,String format) {
+        if(seconds == null || seconds.isEmpty() || seconds.equals("null")){
+            return "";
+        }
+        if(format == null || format.isEmpty()){
+            format = "yyyy-MM-dd HH:mm:ss";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(new Date(Long.valueOf(seconds+"000")));
+    }
+    /**
+     * 日期格式字符串转换成时间戳
+     * @param format 如：yyyy-MM-dd HH:mm:ss
+     * @return
+     */
+    public static String date2TimeStamp(String date_str,String format){
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat(format);
+            return String.valueOf(sdf.parse(date_str).getTime()/1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
